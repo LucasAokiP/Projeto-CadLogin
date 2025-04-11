9c7ae9766c46ee00d9e7a82dd4a7966e7bdc50fe
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-04-11 20:41:08 -0300

    correçao na funçao acessar loginEmail

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //validação de preenchimento de login
 function acessar(){
-    let loginEmail = document.getElementById("loginEmail").Value;
+    let loginEmail = document.getElementById("loginEmail").value;
     let loginSenha = document.getElementById("loginSenha").value;
     
     //verificação se os campos foram preenchidos
