7f8337d332cc697126e63b4e629d00c1373b829a
Lucas Aoki <lucas.aoki2001@gmail.com>
2025-04-11 21:49:16 -0300

    criaçao da função criaLista e adição da funçao cria lista dentro da funçao SalverUser

--- main.js
+++ main.js
@@ -21,9 +21,19 @@ function salvarUser(){
     if(nomeUser){
         dadosLista.push(nomeUser);
         //console.log(dadosLista);
+        criaLista();
         document.getElementById("nomeUser").value = ""; //limpa o label com o nome digitado
     }
     else{
         alert("Usuario não preenchido!!");
+    }
+}
+
+//funçao para criar a lista de nomes
+function criaLista(){
+    let tabela = "<tr><th>Nome</th><th>Ações</th></tr>";
+    for(let i = 0; i <= (dadosLista.length - 1); i++){
+        tabela += "<tr><td>"+dadosLista[i]+"</td><td></td></tr>";
+        document.getElementById("tabela").innerHTML = tabela;
     }
 }
